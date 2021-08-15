3151af91e5bed6556b6e64c157b329e78336919c
poop-hub <76471327+poop-hub@users.noreply.github.com>
2021-08-15 12:11:12 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * rate * years / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    
+    //check the principle for erroneous values
+    if (principal <= 0) {
+      alert("Enter a positive number");
+      document.getElementById("principal").focus();
+    }
+    else {
+      document.getElementById("result").innerHTML =
+        "If you deposit "+"<span style=\"background-color: #FFFF00\">"+principal+"</span>"+
+        ",<br>at an interest rate of "+"<span style=\"background-color: #FFFF00\">"+rate+"</span>"+
+        "%<br>You will receive an amount of "+"<span style=\"background-color: #FFFF00\">"+interest+"</span>"+
+        ",<br>in the year "+"<span style=\"background-color: #FFFF00\">"+year+"</span>"+"<br>"
+    }
+}
+
+function updateRate()
+{
+  var value = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = value + "%";
+}
+        
